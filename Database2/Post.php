a1b2794d5ea143523f92f9379b0286244b79770e
NaveenaAdol <134511283+NaveenaAdol@users.noreply.github.com>
2023-06-08 15:44:26 +0530

    Post.php

--- Database2/Post.php
+++ Database2/Post.php
@@ -9,18 +9,18 @@ class Post {
         $this->db=new Database();
     }
 
-    function getPost(){
+   public function getPost(){
         $this->db->query("SELECT * FROM tbl_oop_post");
         return $this->db->resultset();
     }
 
-    function getPostById($id) {
+    public function getPostById($id) {
         $this->db->query("SELECT * FROM tbl_oop_post WHERE id=:id");
         $this->db->bind(':id',$id);
         return $this->db->single();
     }
 
-    function addPost($data) {
+    public function addPost($data) {
         $this->db->query("INSERT INTO tbl_oop_post (title,content) VALUES (:title,:content)");
         $this->db->bind(':title' , $data['title']);
         $this->db->bind(':content' , $data['content']);
@@ -32,7 +32,7 @@ class Post {
         }
     }
 
-    function updatePost($data) {
+    public function updatePost($data) {
         $this->db->query("UPDATE tbl_oop_post SET title=:title,content=:content where id=:id");
         $this->db->bind(':id' , $data['id']);
         $this->db->bind(':title' , $data['title']);
@@ -45,11 +45,11 @@ class Post {
         }
     }
 
-    function deletePost($id) {
-    $this->db->query("DELETE FROM tbl_oop_post WHERE id= :id");
-    $this->db->bind(':id',$id['id']);
+     public function deletePost($id) {
+         $this->db->query("DELETE FROM tbl_oop_post WHERE id= :id");
+         $this->db->bind(':id',$id['id']);
 
-    if($this->db->execute()) {
+         if($this->db->execute()) {
             return true;
         } else {
             return false;
